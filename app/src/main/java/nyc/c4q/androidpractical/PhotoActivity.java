6004c055017bcf89c5317439f093c95e1cd2cddc
Shantal Taveras <shantaltaveras@ac.c4q.nyc>
2018-02-25 11:09:29 -0500

    skeleton app, initial commit

---
+++ app/src/main/java/nyc/c4q/androidpractical/PhotoActivity.java
@@ -0,0 +1,8 @@
+package nyc.c4q.androidpractical;
+
+/**
+ * Created by Shant on 2/25/2018.
+ */
+
+public class PhotoActivity {
+}
---
+++ app/src/main/java/nyc/c4q/androidpractical/network/DogAPI.java
@@ -0,0 +1,8 @@
+package nyc.c4q.androidpractical.network;
+
+/**
+ * Created by Shant on 2/25/2018.
+ */
+
+public interface DogAPI {
+}
